244784f5e8ff9788da8960d297ace53c8637458e
benboba <benboba@gmail.com>
2018-09-07 18:55:45 +0800

    1.2.4

--- src/slimming/matrix/stringify.ts
+++ src/slimming/matrix/stringify.ts
@@ -1,12 +1,13 @@
 import { IMatrixFunc } from './exec';
 import { shorten } from './shorten';
+import { stringifyFuncVal } from '../utils/stringify-funcval';
 
 export const stringify = (m: IMatrixFunc[], digit1: number, digit2: number, digit3: number): string => {
     let result = '';
     m.forEach((v, i) => {
         const _v = shorten(v, digit1, digit2, digit3);
         if (!_v.noEffect) {
-            result += `${_v.type}(${_v.val.join(',')})`;
+            result += `${_v.type}(${stringifyFuncVal(_v.val)})`;
         }
     });
     return result;
