8a55da75f4a3647607f6ece60c5f048d14422375
benboba <benboba@gmail.com>
2018-12-21 15:41:30 +0800

    1.2.8

---
+++ dist/xml-parser.d.ts
@@ -0,0 +1,72 @@
+export declare enum NodeType {
+    EndTag = -1,
+    Tag = 1,
+    Text = 3,
+    CDATA = 4,
+    OtherSect = 5,
+    OtherDecl = 6,
+    XMLDecl = 7,
+    Comments = 8,
+    Document = 9,
+    DocType = 10
+}
+
+export interface IAttr {
+    name: string;
+    value: string;
+    fullname: string;
+    namespace?: string;
+}
+
+export interface INode {
+    nodeName: string;
+    nodeType: NodeType;
+    namespace?: string;
+    selfClose?: boolean;
+    textContent?: string;
+    readonly attributes: ReadonlyArray<IAttr>;
+    readonly childNodes: ReadonlyArray<INode>;
+    parentNode?: INode;
+    cloneNode(): INode;
+    appendChild(childNode: INode): void;
+    insertBefore(childNode: INode, previousTarget: INode): void;
+    replaceChild(childNode: INode, ...children: INode[]): void;
+    removeChild(childNode: INode): void;
+    hasAttribute(name: string, namespace?: string): boolean;
+    getAttribute(name: string, namespace?: string): string;
+    setAttribute(name: string, value: string, namespace?: string): void;
+    removeAttribute(name: string, namespace?: string): void;
+}
+
+export interface INodeOption {
+    nodeName: string;
+    nodeType: NodeType;
+    namespace?: string;
+    selfClose?: boolean;
+    textContent?: string;
+}
+
+export declare class Node implements INode {
+    constructor(option: INodeOption);
+    nodeName: string;
+    nodeType: NodeType;
+    namespace?: string;
+    selfClose?: boolean;
+    textContent?: string;
+    private _attributes?;
+    readonly attributes: ReadonlyArray<IAttr>;
+    private _childNodes?;
+    readonly childNodes: ReadonlyArray<INode>;
+    parentNode?: INode;
+    cloneNode(): INode;
+    appendChild(childNode: INode): void;
+    insertBefore(childNode: INode, previousTarget: INode): void;
+    replaceChild(childNode: INode, ...children: INode[]): void;
+    removeChild(childNode: INode): void;
+    hasAttribute(name: string, namespace?: string): boolean;
+    getAttribute(name: string, namespace?: string): string;
+    setAttribute(name: string, value: string, namespace?: string): void;
+    removeAttribute(name: string, namespace?: string): void;
+}
+
+export declare function Parser(str: string): Promise<Node>;
